aa0f4de9b0d8756985bd6e8ff9de7dc5c143642c
qqapps18 <qqapps18@gmail.com>
2020-06-20 21:33:53 -0400

    Incorporate the call to open the PDF file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
 void main() {
   runApp(BlessingGridView());
 }
@@ -10,6 +16,38 @@ class BlessingGridView extends StatefulWidget {
 }
 
 class _BlessingGridViewState extends State<BlessingGridView> {
+// variable para el Path del PDF
+  String assetPDFPath = "";
+
+// inicializa el path del PDF
+  @override
+  void initState() {
+    super.initState();
+
+    getFileFromAsset("assets/rev_kidush_Shabaten.pdf").then((f) {
+      setState(() {
+        print('este es el path ' + f.path);
+        assetPDFPath = f.path;
+      });
+    });
+  }
+
+// Future que devueleve el Path completo del PDF
+  Future<File> getFileFromAsset(String asset) async {
+    try {
+      var data = await rootBundle.load(asset);
+      var bytes = data.buffer.asUint8List();
+      var dir = await getApplicationDocumentsDirectory();
+      File file = File("${dir.path}/rev_kidush_Shabaten.pdf");
+
+      File assetFile = await file.writeAsBytes(bytes);
+      return assetFile;
+    } catch (e) {
+      print(e);
+      throw Exception("Error opening asset file");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,7 +65,15 @@ class _BlessingGridViewState extends State<BlessingGridView> {
                 children: <Widget>[
                   Container(
                     child: InkWell(
-                      onTap: () {},
+                      onTap: () {
+//                                 Se abre el PDF
+                        if (assetPDFPath != null) {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return PdfViewPage(path: assetPDFPath);
+                          }));
+                        }
+                      },
                       child: Image.asset(
                         'assets/imgblessingaftereaten.jpg',
                       ),
@@ -43,77 +89,71 @@ class _BlessingGridViewState extends State<BlessingGridView> {
                 ],
               ),
             ),
-            Card(
-              color: Colors.amberAccent,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Container(
-                    child: InkWell(
-                      onTap: () {},
-                      child: Image.asset(
-                        'assets/fondo_bendiciones.png',
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Bendiciones Hijos',
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ),
-            Card(
-              color: Colors.amberAccent,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Container(
-                    child: InkWell(
-                      onTap: () {},
-                      child: Image.asset(
-                        'assets/fondo_bendiciones.png',
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Bendiciones 2',
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ),
-            Card(
-              color: Colors.amberAccent,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Container(
-                    child: InkWell(
-                      onTap: () {},
-                      child: Image.asset(
-                        'assets/fondo_bendiciones.png',
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Bendiciones 3',
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+// Se pone el PDF en pantalla
+class PdfViewPage extends StatefulWidget {
+  final String path;
+
+  const PdfViewPage({Key key, this.path}) : super(key: key);
+
+  @override
+  _PdfViewPageState createState() => _PdfViewPageState();
+}
+
+class _PdfViewPageState extends State<PdfViewPage> {
+  int _totalPages = 0;
+  int _currentPage = 0;
+  bool pdfReady = false;
+  PDFViewController _pdfViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Document"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          PDFView(
+            filePath: widget.path,
+            autoSpacing: true,
+            enableSwipe: true,
+            pageSnap: true,
+            swipeHorizontal: true,
+            nightMode: false,
+            onError: (e) {
+              print(e);
+            },
+            onRender: (_pages) {
+              setState(() {
+                print('Esta es la pagina ' + _pages.toString());
+                _totalPages = _pages;
+                _currentPage = _pages;
+                _pdfViewController.setPage(_pages);
+                print('esta en la pagina a abrir  ' + _pages.toString());
+                pdfReady = true;
+              });
+            },
+            onViewCreated: (PDFViewController vc) {
+              _pdfViewController = vc;
+            },
+            onPageChanged: (int page, int total) {
+              setState(() {});
+            },
+            onPageError: (page, e) {},
+          ),
+          !pdfReady
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Offstage()
+        ],
       ),
     );
   }
