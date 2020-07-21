f53a8e66fccaafa93aa8f0c300fd681929f69322
AvilaPC <qqapps18@gmail.com>
2020-07-21 18:58:40 -0400

    Trying to check por festivities and all the carloads are corrected to call the pdfpage correctly

--- lib/FestivitiesBlessing.dart
+++ lib/FestivitiesBlessing.dart
@@ -32,72 +32,70 @@ class CardLoadFes extends StatelessWidget {
       children: <Widget>[
         Card(
           color: Colors.amberAccent,
-          child: Column(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(500, 13, 17, 50),
-                ),
-                height: 60,
-                child: Stack(
-                  children: <Widget>[
-                    InkWell(
-                      onTap: () async {
-                        print('File Name ' +
-                            getTranslated(context, blessibgFe.fileName));
-                        var filePath = await fileProvider.getAssetByName(
-                            getTranslated(context, blessibgFe.fileName));
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return PdfViewPage(
-                            path: filePath.path,
-                            appBarName: getTranslated(context, blessibgFe.name),
-                          );
-                        }));
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Center(
-                        heightFactor: 5,
-                        child: Text(
-                          getTranslated(context, blessibgFe.name),
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 10,
-                              fontFamily: 'RobotoSlab'),
+          child: InkWell(
+            onTap: () async {
+              print('File Name ' + getTranslated(context, blessibgFe.fileName));
+              var filePath = await fileProvider
+                  .getAssetByName(getTranslated(context, blessibgFe.fileName));
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return PdfViewPage(
+                  path: filePath.path,
+                  appBarName: getTranslated(context, blessibgFe.name),
+                );
+              }));
+            },
+            child: Column(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(500, 13, 17, 50),
+                  ),
+                  height: 60,
+                  child: Stack(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Center(
+                          heightFactor: 5,
+                          child: Text(
+                            getTranslated(context, blessibgFe.name),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 10,
+                                fontFamily: 'RobotoSlab'),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.yellow,
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                  ),
+                  height: 2,
                 ),
-                height: 2,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(500, 13, 17, 50),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(500, 13, 17, 50),
+                  ),
+                  height: 15,
                 ),
-                height: 15,
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                getTranslated(context, blessibgFe.appBarName),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.indigo,
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'RobotoSlab'),
-              ),
-            ],
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  getTranslated(context, blessibgFe.appBarName),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.indigo,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'RobotoSlab'),
+                ),
+              ],
+            ),
           ),
         ),
       ],
--- lib/FoodBlessing.dart
+++ lib/FoodBlessing.dart
@@ -32,72 +32,70 @@ class CardLoadF extends StatelessWidget {
       children: <Widget>[
         Card(
           color: Colors.amberAccent,
-          child: Column(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(500, 13, 17, 50),
-                ),
-                height: 65,
-                child: Stack(
-                  children: <Widget>[
-                    InkWell(
-                      onTap: () async {
-                        print('File Name ' +
-                            getTranslated(context, blessingF.fileName));
-                        var filePath = await fileProvider.getAssetByName(
-                            getTranslated(context, blessingF.fileName));
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return PdfViewPage(
-                            path: filePath.path,
-                            appBarName: getTranslated(context, blessingF.name),
-                          );
-                        }));
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Center(
-                        heightFactor: 4,
-                        child: Text(
-                          getTranslated(context, blessingF.name),
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 10,
-                              fontFamily: 'RobotoSlab'),
+          child: InkWell(
+            onTap: () async {
+              print('File Name ' + getTranslated(context, blessingF.fileName));
+              var filePath = await fileProvider
+                  .getAssetByName(getTranslated(context, blessingF.fileName));
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return PdfViewPage(
+                  path: filePath.path,
+                  appBarName: getTranslated(context, blessingF.name),
+                );
+              }));
+            },
+            child: Column(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(500, 13, 17, 50),
+                  ),
+                  height: 60,
+                  child: Stack(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Center(
+                          heightFactor: 5,
+                          child: Text(
+                            getTranslated(context, blessingF.name),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 10,
+                                fontFamily: 'RobotoSlab'),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.yellow,
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                  ),
+                  height: 2,
                 ),
-                height: 2,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(500, 13, 17, 50),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(500, 13, 17, 50),
+                  ),
+                  height: 15,
                 ),
-                height: 10,
-              ),
-              SizedBox(
-                height: 6,
-              ),
-              Text(
-                getTranslated(context, blessingF.appBarName),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.indigo,
-                    fontSize: 10,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'RobotoSlab'),
-              ),
-            ],
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  getTranslated(context, blessingF.appBarName),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.indigo,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'RobotoSlab'),
+                ),
+              ],
+            ),
           ),
         ),
       ],
