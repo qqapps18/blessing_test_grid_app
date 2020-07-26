7f8fbb028595950361246cb0b53b702dad769a76
AvilaPC <qqapps18@gmail.com>
2020-07-26 15:48:46 -0400

    Start working to check the holiday, with some problems geting the data from android module

--- android/app/src/main/java/com/example/blessingtestgridapp/MainActivity.java
+++ android/app/src/main/java/com/example/blessingtestgridapp/MainActivity.java
@@ -10,6 +10,12 @@ import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
 public class MainActivity extends FlutterActivity {
     String leapyear;
     ArrayList<String> listdates = new ArrayList<String>();
@@ -36,6 +42,31 @@ public class MainActivity extends FlutterActivity {
                                     leapyear = "0";
                                     listdates.add(leapyear);
                                 }
+
+                                calendar= Calendar.getInstance();
+                                tday = calendar.get(Calendar.DAY_OF_MONTH);
+                                tmonth = calendar.get(Calendar.MONTH);
+                                tyear = calendar.get(Calendar.YEAR);
+
+                                DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+
+/* ------------------- fecha dia anterior (ayer) ----------------------------- */
+                                Date diayurzaityesterday = new Date((tday - 1900), (tmonth - 1), (tday - 1));
+                                hebrewDateYurzaitString = dateFormat.format(diayurzaityesterday);
+
+                                HebrewDate hebrewDateYurzaityesterday = new HebrewDate(diayurzaityesterday);
+                                listdates.add(Integer.toString(HebrewDate.CURRENT_HDAY));
+                                listdates.add(HebrewDate.CURRENT_HMONTH);
+                                listdates.add(Integer.toString(HebrewDate.CURRENT_HYEAR));
+
+/* ------------------- fecha dia 2 antes(antiayer) ----------------------------- */
+                                Date diayurzaitdaybefore = new Date((tday - 1900), (tmonth - 1), (tday - 2));
+                                hebrewDateYurzaitString = dateFormat.format(diayurzaitdaybefore);
+
+                                HebrewDate hebrewDateYurzaitdaybafore = new HebrewDate(diayurzaitdaybefore);
+                                listdates.add(Integer.toString(HebrewDate.CURRENT_HDAY));
+                                listdates.add(HebrewDate.CURRENT_HMONTH);
+                                listdates.add(Integer.toString(HebrewDate.CURRENT_HYEAR));
 
                                 System.out.println("[ANDRES] In Android  " + listdates);
 
